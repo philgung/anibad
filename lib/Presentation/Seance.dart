df234c258e11463ceeac41ac8b45c031ed8ce375
Philippe Gung <philippe.gung@youscribe.com>
2021-04-13 20:06:44 +0200

    ajout Card & ListTile

--- lib/Presentation/Seance.dart
+++ lib/Presentation/Seance.dart
@@ -8,20 +8,64 @@ class Seance extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Anibad')),
-      body: Center(
-          child: Text(
-        '$index/\n' +
-            'Thème : Déplacement Avant-Arrière\n' +
-            'Objectifs : Position du pied sur le déplacement\n' +
-            'Echauffement:\n' +
-            '- 5 min footing autour du plateau (commentaire : réduit à 2/3 tours)\n' +
-            '- Déverrouillage articulaire\n' +
-            'Corps\n' +
-            '\n' +
-            '\n',
-        style: TextStyle(fontSize: 18),
-      )),
-    );
+        appBar: AppBar(title: Text('Anibad')),
+        body: ListView(
+          children: const <Widget>[
+            Card(
+                child: ListTile(
+                    leading: FlutterLogo(),
+                    title: Text('Thème : Déplacement Avant-Arrière'))),
+            Card(
+              child: ListTile(
+                title: Text(
+                  'Objectifs : Position du pied sur le déplacement',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text(
+                  'Echauffement',
+                  style: TextStyle(color: Colors.blue),
+                ),
+                dense: true,
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.timer),
+                title: Text('5 min footing autour du plateau'),
+                subtitle: Text('(commentaire : réduit à 2/3 tours)'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('Déverrouillage articulaire'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('Corps', style: TextStyle(color: Colors.blue)),
+                dense: true,
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text(
+                    'Routine Avant-Arriere (demi terrain) Court/Court/Long'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('Match Suédois'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+          ],
+        ));
   }
 }
